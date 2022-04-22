ee6555d75cded5ef2f97ed718bac5c342761349d
Dipen Thapa <nepidtube@gmail.com>
2022-04-23 02:28:40 +0545

    complete

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,36 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({});
+  const { featured, company, name, sort, fields } = req.query;
+  const queryObject = {};
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+  if (company) {
+    queryObject.company = company;
+  }
+  if (name) {
+    queryObject.name = { $regx: name, $options: "i" };
+  }
+
+  let products = Product.find(queryObject);
+
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    products = products.sort(sortList);
+  } else {
+    products = products.sort("createdAt");
+  }
+
+  if (fields) {
+    const fieldsList = fields.split(",").join(" ");
+    products = products.select(fieldsList);
+  }
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+  products = products.limit(limit).skip(skip);
+
   res.status(200).json({ msg: products });
 };
 
